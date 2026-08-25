c95bca35f3e7c5e03a00df37e7edc185d201c3a3
GiovanniBSonaglio <giovannibsonaglio@gmail.com>
2026-08-25 12:05:13 +0200

    feat: added uart Rx and Tx unit testing

--- tests/uart/src/main.c
+++ tests/uart/src/main.c
@@ -1,19 +1,55 @@
-#include <zephyr/ztest.h>
+#include <string.h>
 
-ZTEST_SUITE(uart_unit_test, NULL, NULL, NULL, NULL, NULL);
+#include <zephyr/ztest.h>
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/serial/uart_emul.h>
+
+#include "uart.h"
+
+static const struct device *const emul_dev = DEVICE_DT_GET(DT_NODELABEL(uart_test));
+
+static void uart_before(void *fixture)
+{
+    ARG_UNUSED(fixture);
+    uart_emul_flush_rx_data(emul_dev);
+    uart_emul_flush_tx_data(emul_dev);
+    zassert_equal(init_uart(emul_dev), 0, "Error initializing UART");
+}
+ZTEST_SUITE(uart_unit_test, NULL, NULL, uart_before, NULL, NULL);
 
 /**
- * @brief Test Asserts
+ * @brief Test Uart Tx
  *
- * This test verifies various assert macros provided by ztest.
+ * This test verifies if the uart tx is working.
  *
  */
-ZTEST(uart_unit_test, test_assert)
+ZTEST(uart_unit_test, test_uart_tx)
 {
-	zassert_true(1, "1 was false");
-	zassert_false(0, "0 was true");
-	zassert_is_null(NULL, "NULL was not NULL");
-	zassert_not_null("foo", "\"foo\" was NULL");
-	zassert_equal(1, 1, "1 was not equal to 1");
-	zassert_equal_ptr(NULL, NULL, "NULL was not equal to NULL");
+	uint8_t buf[4];
+	uint8_t *data = &buf[0];
+	int data_len;
+
+	uart_write("test", 4, K_FOREVER);
+	data_len = uart_emul_get_tx_data(emul_dev, data, 4);
+	zassert_equal(data_len, 4, "Incorrect written data length");
+	zassert_mem_equal(data, "test", 4);
+}
+
+/**
+ * @brief Test Uart Rx
+ *
+ * This test verifies if the uart Rx is working.
+ *
+ */
+ZTEST(uart_unit_test, test_uart_rx)
+{
+	char test_str[] = "test";
+	uint8_t buf;
+
+	uart_emul_put_rx_data(emul_dev, "test", 4);
+	
+	for(uint8_t i = 0; i < strlen(test_str); i++) {
+		uart_get_byte(&buf, K_FOREVER);
+		zassert_equal(buf, test_str[i], "Character in pos %d does not match", i);
+	}
 }
